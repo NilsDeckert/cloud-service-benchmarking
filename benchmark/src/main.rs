5a2401be35fa3e7fa979a7226216065bf897dff0
Nils Deckert <nils@deckert.dev>
2025-12-22 16:29:47 +0100

    Start load generator

--- benchmark/src/main.rs
+++ benchmark/src/main.rs
@@ -1,10 +1,15 @@
 use std::time::Duration;
-
 use redis::{RedisResult, Value};
+
+use crate::load_generator::load_generator::LoadGenerator;
+
+mod load_generator;
+
+const NUM_REQ: u32 = 1000;
 
 extern crate redis;
 
-fn test_connection(client: &redis::Client, name: &str) -> RedisResult<()>{
+fn test_connection(client: &redis::Client, name: &str) -> RedisResult<redis::Connection>{
     let mut con = client.get_connection_with_timeout(Duration::new(3,0))?;
     let mut cmd = redis::Cmd::new();
 
@@ -19,9 +24,10 @@ fn test_connection(client: &redis::Client, name: &str) -> RedisResult<()>{
         }
     }
 
-    Ok(())
+    Ok(con)
 }
 
+#[allow(unused)]
 fn main() {
     let redis_client  = redis::Client::open("redis://34.163.77.237/").expect("Failed to create redis client");
     let valkey_client = redis::Client::open("redis://34.155.91.70/").expect("Failed to create valkey client");
@@ -29,8 +35,13 @@ fn main() {
     let keydb_client  = redis::Client::open("redis://34.163.49.76/").expect("Failed to create keydb client");
 
     println!("Testing connections...");
-    let _ = test_connection(&redis_client,  "redis");
-    let _ = test_connection(&valkey_client, "valkey");
-    let _ = test_connection(&acdis_client,  "acdis");
-    let _ = test_connection(&keydb_client,  "keydb");
+    // let mut redis_con = test_connection(&redis_client,  "redis").expect("Connection failed");
+    // let mut valkey_con = test_connection(&valkey_client, "valkey").expect("Connection failed");
+    let mut acdis_con = test_connection(&acdis_client,  "acdis").expect("Connection failed");
+    // let mut keydb_con = test_connection(&keydb_client,  "keydb").expect("Connection failed");
+
+    let mut lg = LoadGenerator::new(0, 100, 0, 100).expect("Error creating load generator");
+    for _ in 0..NUM_REQ {
+        lg.cmd_get().query::<redis::Value>(&mut acdis_con);
+    }
 }
