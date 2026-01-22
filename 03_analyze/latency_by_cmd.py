bc5f1e7bf9556207de010993b5d49d5186d05c18
Nils Deckert <nils@deckert.dev>
2026-01-22 15:15:11 +0100

    add benchmark data

--- 03_analyze/latency_by_cmd.py
+++ 03_analyze/latency_by_cmd.py
@@ -12,37 +12,56 @@ dfs = []
 
 # Collect csv from given folders
 for folder in folders:
-    service = os.path.dirname(folder)
+    service = folder.rstrip("/").split("/")[-1]
+    service.rstrip(".csv")
+    print(f"Service: {service}")
     files = glob.glob(f"{folder}/*.csv")
+
+    service_list = []
 
     for file in files:
         df = pd.read_csv(file, comment="#")
         df["service"] = service
-        df["node"] = file
+        df["node"] = os.path.basename(file).rstrip(".csv")
         # "Explode" csv. Repeat each latency 'count' times.
         df = df.loc[df.index.repeat(df['count'])]
         print(f"{file} has {len(df)} entries")
 
-        dfs.append(df)
+        service_list.append(df)
+
+    df = pd.concat(service_list, ignore_index=True)
+    dfs.append(df)
+
+    ### Put analysis per project here
+    plt.figure(figsize=(10, 6))
+    boxplot = sns.boxplot(x="cmd", y="latency", data=df, showfliers=False)
+    boxplot.set_title(f"Latency by command {service}")
+    plt.savefig(f"images/{service}_latency_by_command.png")
+    plt.close()
 
 # Combine all dataframe into one
 df = pd.concat(dfs, ignore_index=True)
+print(df.head())
 
 print("=== BY SERVICE ===")
 plt.figure(figsize=(10, 6))
-sns.boxplot(x="service", y="latency", data=df)
-plt.show()
+summary = sns.boxplot(x="service", y="latency", data=df, showfliers=False)
+summary.set_title("Combined latency per service")
+plt.savefig("images/summary_latency.png")
+plt.close()
 
-print("=== BY NODE ===")
+print("=== BY BENCHER ===")
 print(df.groupby(["node"]).describe())
 plt.figure(figsize=(10, 6))
-sns.boxplot(x="node", y="latency", data=df)
-plt.show()
+validation = sns.boxplot(x="node", y="latency", data=df, showfliers=False)
+plt.savefig("images/validation_bencher.png")
+plt.close()
 
-print("\n=== BY CMD ===")
-print(df.groupby(["cmd"])["latency"].describe())
-sns.boxplot(x="cmd", y="latency", data=df)
-plt.show()
+# print("\n=== BY CMD ===")
+# print(df.groupby(["cmd"])["latency"].describe())
+# sns.boxplot(x="cmd", y="latency", data=df, showfliers=False)
+# plt.show()
+
 #
 #     print()
 #     print(df["thread"] + 5)
