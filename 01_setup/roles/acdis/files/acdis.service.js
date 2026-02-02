d98edff9ae15104655de0b7cd995a210fdef2880
Nils Deckert <nils@deckert.dev>
2026-02-02 18:01:34 +0100

    Fix service file

--- 01_setup/roles/acdis/files/acdis.service.js
+++ 01_setup/roles/acdis/files/acdis.service.js
@@ -4,7 +4,7 @@ After=network-online.target
 Wants=network-online.target systemd-networkd-wait-online.service
 
 [Service]
-ExecStart=/opt/acdis/{{ acdis_bin }}
+ExecStart=/opt/acdis/acdis --own {{ ansible_host }} --manager {{ hostvars['acdis1']['ansible_host'] }}
 
 [Install]
 WantedBy=multi-user.target
